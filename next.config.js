120ec54ea2da8d2e55288e9e779b23de52de5966
SafnasKaldeen <safnaskm.20@uom.lk>
2025-11-29 17:00:22 +0530

    config

--- next.config.js
+++ next.config.js
@@ -6,17 +6,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
-  ...(process.env.NODE_ENV === "production"
-    ? { output: "export", trailingSlash: true }
-    : {}),
-  // TODO: Removing this currently breaks the export with Next 15.2.4.
-  // Revisit when Next.js fixes ESM externals handling for static export.
   experimental: {
     esmExternals: false,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
