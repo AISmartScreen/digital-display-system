778804dc0134333e93e9d59ad56bd50ad819dab5
SafnasKaldeen <safnaskm.20@uom.lk>
2025-12-30 13:59:02 +0530

    font ussue

--- components/templates/masjid-template.tsx
+++ components/templates/masjid-template.tsx
@@ -2031,7 +2031,7 @@ export function MasjidTemplate({
                 {/* Time with enhanced typography */}
                 <div className="mb-6 mt-4">
                   <p
-                    className={`text-[${customization.fontSize}] font-black leading-[0.9] tracking-wide tabular-nums`}
+                    className={`text-[${customization.fontSize}rem] font-black leading-[0.9] tracking-wide tabular-nums`}
                     style={{
                       color: customization.colors.text,
                       textShadow: `
